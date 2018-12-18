47e672d2f10a805124fd86cd755a9739e3238660
Holden Altaffer <haltaffer@gmail.com>
2018-12-18 00:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ class Driver {
         store.drivers.push(this);
     }
 
+    trips() {
+      return store.trips.filter(
+            function(trip) {
+                return trip.driverId === this.id;
+            }.bind(this))
+      };
+
 
   }
 
