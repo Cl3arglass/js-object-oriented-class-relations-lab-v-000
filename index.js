c7699a636f0c68f4606f0e5a75001315971d677d
Holden Altaffer <haltaffer@gmail.com>
2018-12-18 00:37:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ class Trip {
         store.trips.push(this);
     }
 
-    passenger() {
-      return store.passengers.find(
-              function(passenger) {
-                  return passenger.id === this.passengerId;
-              }.bind(this)
-      };
+    // passenger() {
+    //   return store.passengers.find(
+    //           function(passenger) {
+    //               return passenger.id === this.passengerId;
+    //           }.bind(this)
+    //   };
     }
 
   }
