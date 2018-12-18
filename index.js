169785f19f8048f14241b2bc22dd0ec5b39ed0ec
Holden Altaffer <haltaffer@gmail.com>
2018-12-18 00:37:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ class Trip {
     }
 
     passenger() {
-      return store.passengers.filter(
-            function(passenger) {
-                return passenger.tripId === this.id;
-            }.bind(this)
+      return store.passengers.find(
+              function(passenger) {
+                  return passenger.id === this.passengerId;
+              }.bind(this)
       };
     }
 
