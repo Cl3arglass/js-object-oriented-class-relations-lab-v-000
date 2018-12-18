2c105734d5d72e8fe81cd42c68a54a145909230f
Holden Altaffer <haltaffer@gmail.com>
2018-12-18 00:04:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+let store = { drivers: [], trips: [], passengers: [] };
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+
+        store.drivers.push(this);
+    }
+  }
+
+class Passenger {
+    constructor(name) {
+        this.id = ++passengerId;
+        this.name = name;
+
+        store.passengers.push(this);
+    }
+  }
+
+class Trip {
+    constructor() {
+        this.id = ++tripId;
+
+        store.trips.push(this);
+    }
+  }
